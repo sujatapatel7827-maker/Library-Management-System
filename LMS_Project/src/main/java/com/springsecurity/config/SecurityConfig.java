1e907550eb58f697fec0703f2238c502d41dc1e6
Sujata <sujatapatel7827@gmail.com>
2026-06-11 13:49:56 +0530

    Fix CORS issue with allowedOriginPatterns

--- LMS_Project/src/main/java/com/springsecurity/config/SecurityConfig.java
+++ LMS_Project/src/main/java/com/springsecurity/config/SecurityConfig.java
@@ -78,7 +78,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList(allowedOrigins));
+        configuration.setAllowedOriginPatterns(Arrays.asList(allowedOrigins));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Cache-Control"));
         configuration.setExposedHeaders(List.of("Authorization"));
